86da22464da7c80f706af5b817e7dad969057569
augustosalazar <augustosalazar@uninorte.edu.co>
2020-08-06 21:56:41 -0500

    Add basic menu option

--- app/src/main/java/com/uninorte/navigationandmenu/MainActivity.kt
+++ app/src/main/java/com/uninorte/navigationandmenu/MainActivity.kt
@@ -2,10 +2,32 @@ package com.uninorte.navigationandmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_favorite -> {
+            Log.d("NavigationAndMenuDebug","action_favorite")
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
     }
 }
